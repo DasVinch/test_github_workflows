03b76698173437534301f11dbd32e502bfb49e64
vdeo <vdeo@naoj.org>
2026-03-12 12:46:56 +0100

    play with workflows

--- src/mylib.c
+++ src/mylib.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "mylib.h"
+#include <stdio.h>
 
 #define BUF_SIZE 256
 
 static _Thread_local char buf[BUF_SIZE];
 
-const char *greet(const char *name) {
+const char* greet(const char* name) {
     snprintf(buf, BUF_SIZE, "Hello, %s!", name);
     return buf;
 }
--- src/mylib.h
+++ src/mylib.h
@@ -2,6 +2,6 @@
 #define MYLIB_H
 
 /* Returns a greeting string for the given name. */
-const char *greet(const char *name);
+const char* greet(const char* name);
 
 #endif /* MYLIB_H */
