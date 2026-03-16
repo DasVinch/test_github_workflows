c83cb84a491ab530fdf65a54cac49bb6e612ff8d
vdeo <vdeo@naoj.org>
2026-03-16 21:56:33 +0100

    more testing

--- src/mylib.c
+++ src/mylib.c
@@ -6,6 +6,7 @@
 static _Thread_local char buf[BUF_SIZE];
 
 const char* greet(const char* name) {
-    snprintf(buf, BUF_SIZE, "Hello, %s!", name);
+    snprintf(buf, 
+        BUF_SIZE, "Hello, %s!", name);
     return buf;
 }
